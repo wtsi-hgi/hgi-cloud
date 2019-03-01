91cce5227ef198ff0db10097e79a4821ae10caec
Luca De Vitis <ld14@sanger.ac.uk>
2019-03-01 15:19:42 +0000

    Now creates a server group with floating IPs

--- terraform/openstack/modules/clusters/main.tf
+++ terraform/openstack/modules/clusters/main.tf
@@ -1,42 +1,42 @@
-data "openstack_networking_network_v2" "sanger_internal_openstack_zeta_hgi_cluster_network" {
-  name     = "${var.cluster_network_name}"
-  external = true
-}
-
 resource "openstack_compute_servergroup_v2" "cluster" {
-  name      = "uk_sanger_internal_openstack_zeta_${var.region}_${var.env}_hgi_cluster_${var.name}"
-  region    = "${var.region}"
-  policies  = ["soft-anti-affinity"]
+  name      = "uk_sanger_internal_openstack_zeta_${var.env}_hgi_cluster_${var.role}"
+  policies  = ["${var.affinity}"]
 }
 
-resource "openstack_compute_floatingip_v2" "floatingip" {
+resource "openstack_compute_floatingip_v2" "public" {
   pool  = "${var.subnetpool_name}"
   count = "${var.count}"
 }
 
 resource "openstack_compute_instance_v2" "instance" {
-  name            = "uk_sanger_internal_openstack_zeta_${var.region}_${var.env}_hgi_cluster_${var.name}_${count.index}"
+  name            = "uk_sanger_internal_openstack_zeta_${var.env}_hgi_cluster_${var.role}_${count.index + 1}"
   count           = "${var.count}"
   image_name      = "${var.image_name}"
   flavor_name     = "${var.flavor_name}"
   key_pair        = "${var.key_pair}"
-  security_groups = "${var.security_groups}"
-  floating_ip     = "${openstack_compute_floatingip_v2.floating_ip.*.address[count.index]}"
+  security_groups = [ "${var.security_groups}" ]
 
   network {
-    uuid = "${var.network_uuid}"
+    name = "${var.network_name}"
   }
 
-  provisioner "remote-exec" {
-    connection {
-      user = "${var.ssh_user_name}"
-      key_file = "${var.ssh_key_file}"
-    }
+  # provisioner "remote-exec" {
+  #   connection {
+  #     user = "${var.ssh_user_name}"
+  #     key_file = "${var.ssh_key_file}"
+  #   }
 
-    inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get -y install nginx",
-      "sudo service nginx start"
-    ]
-  }
+  #   inline = [
+  #     "sudo apt-get -y update",
+  #     "sudo apt-get -y install nginx",
+  #     "sudo service nginx start"
+  #   ]
+  # }
+}
+
+resource "openstack_compute_floatingip_associate_v2" "associate" {
+  count = "${var.count}"
+  floating_ip = "${element(openstack_compute_floatingip_v2.public.*.address, count.index)}"
+  instance_id = "${element(openstack_compute_instance_v2.instance.*.id, count.index)}"
+
 }
