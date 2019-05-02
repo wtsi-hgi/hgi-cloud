15c4a0eb1990a43e8c044dafa841e7519e2fefbe
Luca De Vitis <ld14@sanger.ac.uk>
2019-05-02 13:19:39 +0000

    python lint

--- ansible/roles/hail-base/molecule/default/tests/test_default.py
+++ ansible/roles/hail-base/molecule/default/tests/test_default.py
@@ -13,19 +13,23 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_ipython(host):
-  # ipython <<IPYTHON
-  # import hail as hl
-  # mt = hl.balding_nichols_model(n_populations=3, n_samples=50, n_variants=100)
-  # mt.count()
-  # IPYTHON
-  assert True
+    # ipython <<IPYTHON
+    # import hail as hl
+    # mt = hl.balding_nichols_model(n_populations=3, n_samples=50,
+    #                               n_variants=100)
+    # mt.count()
+    # IPYTHON
+    assert True
+
 
 def test_pyspark(host):
-  # pyspark ${PYSPARK_ARGS} <<PYSPARK
-  # import hail as hl
-  # hl.init(sc)
-  # mt = hl.balding_nichols_model(n_populations=3, n_samples=50, n_variants=100)
-  # mt.count()
-  # PYSPARK
-  assert True
+    # pyspark ${PYSPARK_ARGS} <<PYSPARK
+    # import hail as hl
+    # hl.init(sc)
+    # mt = hl.balding_nichols_model(n_populations=3, n_samples=50,
+    #                               n_variants=100)
+    # mt.count()
+    # PYSPARK
+    assert True
