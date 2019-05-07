e290628f9072864ec63a07a9666cfdfccb156a22
Luca De Vitis <ld14@sanger.ac.uk>
2019-05-07 08:48:12 +0000

    Fixed object name

--- terraform/modules/openstack/infrastructure/instances/extra/fixed_ip_port/main.tf
+++ terraform/modules/openstack/infrastructure/instances/extra/fixed_ip_port/main.tf
@@ -19,7 +19,7 @@ data "openstack_networking_subnet_v2" "subnet" {
 }
 
 resource "openstack_networking_port_v2" "port" {
-  name                = "uk-sanger-internal-openstack-${var.os_release}-${var.programme}-${var.env}-port-${var.deployment_name}-${var.role_name}-${var.network_name}-${format("%02d", count.index + 1)}"
+  name                = "uk-sanger-internal-openstack-${var.os_release}-${var.programme}-${var.env}-port-${var.network_name}-${var.deployment_name}-${var.role_name}-${format("%02d", count.index + 1)}"
   count               = "${var.count}"
   network_id          = "${data.openstack_networking_network_v2.network.id}"
   admin_state_up      = "true"
