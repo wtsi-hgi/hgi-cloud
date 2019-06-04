e6d62c588a2e9293936a1f80792ccb6e645ee72b
Luca De Vitis <ld14@sanger.ac.uk>
2019-06-04 16:27:05 +0000

    Simplified usage

--- invoke.sh
+++ invoke.sh
@@ -9,52 +9,54 @@ die() {
   exit $1
 }
 
-declare -A COLLECTION
-COLLECTION[image]="packer"
-COLLECTION[role]="molecule"
-COLLECTION[deployment]="terraform"
+# User needs to source the right openrc.sh file.
+if [ -z "${OS_PROJECT_NAME}" ] ; then
+  die 1 "OS_PROJECT_NAME is empty: you need to source the right openrc.sh file"
+fi
 
-TYPE="${1}"
-shift
+source "os_projects/${OS_PROJECT_NAME}.rc"
 
-case "${TYPE}" in
+case "${1}" in
   --help)
     cat <<HELP
 SYNOPSIS
 
   ${0} --help
-  ${0} image|role NAME/VERSION [invoke_options] [ [task_name] [task_options] ... ]
-  ${0} deployment NAME/OWNER [invoke_options] [ [task_name] [task_options] ... ]
+  ${0} [image | role] NAME/VERSION [invoke_options] [ [task_name] [task_options] ... ]
+  ${0} deployment [NAME | NAME/OWNER] invoke_options] [ [task_name] [task_options] ... ]
 
 DESCRIPTION
   Runs automation tasks on a specific types of objects within the
   infrastructure. The task names depend on the type of the object.
+  If OWNER is not specified, the value of OS_USERNAME will be used.
 
   --help                          Prints this help message. Any other argument
                                   will be discarded.
 EXAMPLES
 
-  $ bash invoke.sh deployment consul/defalut up
+  $ bash invoke.sh deployment consul up
   $ bash invoke.sh deployment spark-cluster/vvi plan --to update
-  $ bash invoke.sh role hail-base/HEAD test
-  $ bash invoke.sh image base/0.1.0 build
+  $ bash invoke.sh role hail-base:HEAD test
+  $ bash invoke.sh image base:0.1.0 build
 HELP
     exit 0
   ;;
   image|role)
-    IFS="/" read INVOKE_ROLE_NAME INVOKE_ROLE_VERSION <<<"${1}"
+    IFS="/" read INVOKE_ROLE_NAME INVOKE_ROLE_VERSION <<<"${2}"
     export INVOKE_ROLE_NAME INVOKE_ROLE_VERSION
   ;;
   deployment)
-    IFS="/" read INVOKE_DEPLOYMENT_NAME INVOKE_DEPLOYMENT_OWNER <<<"${1}"
-    export INVOKE_DEPLOYMENT_NAME INVOKE_DEPLOYMENT_OWNER
+    IFS="/" read INVOKE_DEPLOYMENT_NAME DEPLOYMENT_OWNER <<<"${2}"
+    export INVOKE_DEPLOYMENT_NAME
+    export INVOKE_DEPLOYMENT_OWNER="${DEPLOYMENT_OWNER:-"${OS_USERNAME}"}"
   ;;
   *)
-    die 1 "Sorry, cannot automate \`${TYPE}'"
+    die 1 "Sorry, cannot automate \`${$1}'"
   ;;
 esac
 
-shift
+COLLECTION="${1}"
+shift 2
 
 # Creates python3's virtualenv
 if [ ! -d "${PWD}/py3" ] ; then
@@ -83,18 +85,11 @@ fi
 # Install all python modules
 pip install --requirement requirements.txt
 
-# User needs to source the right openrc.sh file.
-if [ -z "${OS_PROJECT_NAME}" ] ; then
-  die 1 "OS_PROJECT_NAME is empty: you need to source the right openrc.sh file"
-fi
-
-source "os_projects/${OS_PROJECT_NAME}.rc"
-
 # These variables will describe the exact cloud / project in which to operate, in invoke
-export INVOKE_META_PROGRAMME="${META_PROGRAMME:-hgi}"
-export INVOKE_META_ENV="${META_ENV:-dev}"
-export INVOKE_META_PROVIDER="${META_PROVIDER:-openstack}"
-export INVOKE_META_DATACENTER="${META_DATACENTER:-eta}"
+export INVOKE_META_PROGRAMME="${META_PROGRAMME:?"META_PROGRAMME is null or unset"}"
+export INVOKE_META_ENV="${META_ENV:?"META_ENV is null or unset"}"
+export INVOKE_META_PROVIDER="${META_PROVIDER:?"META_PROVIDER is null or unset"}"
+export INVOKE_META_DATACENTER="${META_DATACENTER:?"META_DATACENTER is null or unset"}"
 
 # Runs the actual invoke command
-invoke --collection "tasks/${COLLECTION[${TYPE}]}" "${@}"
+invoke --collection "tasks/${COLLECTION}" "${@}"
