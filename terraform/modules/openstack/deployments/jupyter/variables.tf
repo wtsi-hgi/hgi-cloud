8161697a0a699f3b3b8287c3fe9cbdf91f6bb016
Luca De Vitis <ld14@sanger.ac.uk>
2019-06-12 16:05:50 +0000

    Introduced jupyter data sotrage deployment

---
+++ terraform/modules/openstack/deployments/jupyter/variables.tf
@@ -0,0 +1,16 @@
+variable "datacenter" {}
+variable "programme" {}
+variable "env" {}
+variable "deployment_name" {}
+variable "deployment_owner" {}
+variable "size" {
+  default = 1
+}
+variable "depends_on" {
+  default = ""
+}
+variable "count" {
+  default = 1
+}
+
+
