e4e6f4504a1be512ec7a84d2c279a5debc069427
Luca De Vitis <ld14@sanger.ac.uk>
2019-06-19 11:01:01 +0000

    Ignore debconf dialogs

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -eux
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Install Ansible repository.
-apt-get --assume-yes update && apt-get --assume-yes upgrade
-apt-get --assume-yes install software-properties-common
+apt-get --assume-yes --quiet update && apt-get --assume-yes --quiet upgrade
+apt-get --assume-yes --quiet install software-properties-common
 apt-add-repository --yes ppa:ansible/ansible
 
 # Install Ansible.
-apt-get --assume-yes update
-apt-get --assume-yes install ansible python-apt
+apt-get --assume-yes --quiet update
+DEBIAN_FRONTEND=noninteractive apt-get --assume-yes --quiet install ansible python-apt
 mkdir --parents /opt/sanger.ac.uk/ansible/{log,facts.d}
 chown --recursive ubuntu:ubuntu /opt/sanger.ac.uk
 
