24c613a2b95eb5f12a88dbecbd3203e3fe79a162
Luca De Vitis <ld14@sanger.ac.uk>
2019-06-24 16:10:01 +0000

    image tasks touch-up

--- tasks/image.py
+++ tasks/image.py
@@ -10,6 +10,8 @@ import keystoneauth1.identity.v3
 import keystoneauth1.session
 import keystoneclient.v3
 
+import glanceclient
+
 auth = keystoneauth1.identity.v3.Password(user_domain_name=os.environ['OS_USER_DOMAIN_NAME'],
                                           username=os.environ['OS_USERNAME'],
                                           password=os.environ['OS_PASSWORD'],
@@ -17,7 +19,9 @@ auth = keystoneauth1.identity.v3.Password(user_domain_name=os.environ['OS_USER_D
                                           project_name=os.environ['OS_PROJECT_NAME'],
                                           auth_url=os.environ['OS_AUTH_URL'])
 session = keystoneauth1.session.Session(auth=auth)
+
 keystone = keystoneclient.v3.client.Client(session=session)
+glance = glanceclient.Client('2', session=session)
 
 openstack = openstack_client.connect(auth_url=os.environ['OS_AUTH_URL'],
                                      project_name=os.environ['OS_PROJECT_NAME'],
@@ -28,45 +32,43 @@ openstack = openstack_client.connect(auth_url=os.environ['OS_AUTH_URL'],
 
 now = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
 
-def role_version(context):
+def default_version(context):
   config_version = context.config['role']['version']
   return now if (config_version == '0.0.0') else config_version
 
-def packer_options(context):
+def packer_options(context, role_name, role_version=None):
   network_template = '{}-{}-{}-network-main'
   network_name = network_template.format(context.config['meta']['datacenter'],
                                          context.config['meta']['programme'],
                                          context.config['meta']['env'])
   network_id = openstack.network.find_network(network_name).id
-  var_file = 'vars/{}-{}-{}.json'.format(context.config['meta']['datacenter'],
-                                         context.config['meta']['programme'],
-                                         context.config['meta']['env'])
+  var_file = 'vars/{}-{}.json'.format(context.config['meta']['datacenter'],
+                                      context.config['meta']['programme'])
   return ' '.join([
     '-var "network_id={}"'.format(network_id),
-    '-var "role_name={}"'.format(context.config['role']['name']),
-    '-var "role_version={}"'.format(role_version(context)),
+    '-var "role_name={}"'.format(role_name),
+    '-var "role_version={}"'.format(role_version or default_version(context)),
     '-var-file={}'.format(var_file)
   ])
 
-def get_image(context):
+def get_image(context, role_name, role_version):
   image_name = '-'.join([context.config['meta']['datacenter'],
                          context.config['meta']['programme'],
-                         'image',
-                         context.config['role']['name'],
-                         role_version(context)])
+                         'image', role_name, role_version or default_version(context)])
   return openstack.image.find_image(image_name)
 
 
 @invoke.task()
 def validate(context):
   with context.cd('packer'):
-    context.run('packer validate {} image.json'.format(packer_options(context)))
+    context.run('packer validate {} image.json'.format(packer_options(context, 'validate', '0.0.0')))
 
-@invoke.task(validate, optional=['force', 'on_error', 'debug'])
-def build(context, on_error='cleanup', force=False, debug=False):
-  image = get_image(context)
+@invoke.task(validate, optional=['version', 'force', 'on_error', 'debug'])
+def build(context, role_name, role_version=None, on_error='cleanup', force=False, debug=False):
+  image = get_image(context, role_name, role_version)
   if not image:
-    options = '{} -on-error={}'.format(packer_options(context), on_error)
+    options = packer_options(context, role_name, role_version) + \
+              ' -on-error={}'.format(on_error)
     if force:
       options += ' -force'
     if debug:
@@ -76,20 +78,27 @@ def build(context, on_error='cleanup', force=False, debug=False):
   else:
     print("Skipping {}: image is already available".format(image.name))
 
-@invoke.task(build)
-def publish(context):
-  # user_id = openstack.identity.get_user_id()
-  image_id = get_image(context).id
-  for project_id in context.config['image']['publish'].values():
-    context.run('openstack image add project {} {}'.format(image_id, project_id))
+@invoke.task(pre=[build])
+def promote(context, to, role_name, role_version):
+  user = openstack.identity.get_user_id()
+  image_id = get_image(context, role_name, role_version).id
+  project = next((p for p in keystone.projects.list(user=user) if p.name == to))
+  glance.image_members.create(image_id, project.id)
+  glance.image_members.update(image_id, project.id, 'accepted')
 
 @invoke.task()
-def accept(context):
-  image_id = get_image(context).id
-  context.run('openstack image set --accept {}'.format(image_id))
+def accept(context, image_name):
+  image_id = openstack.image.find_image(image_name).id
+  glance.image_members.update(image_id, os.environ['OS_PROJECT_ID'], 'accepted')
+
+@invoke.task()
+def share(context, with_, role_name, role_version):
+  image_id = get_image(context, role_name, role_version).id
+  glance.image_members.create(image_id, with_)
 
 ns = invoke.Collection()
 ns.add_task(validate)
-ns.add_task(build)
-ns.add_task(publish, default=True)
+ns.add_task(build, default=True)
+ns.add_task(promote)
+ns.add_task(share)
 ns.add_task(accept)
