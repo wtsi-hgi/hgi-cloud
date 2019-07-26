8fd02feec952a0202f6f0fbea9c6aa46092ccb67
Luca De Vitis <lucadevitis@users.noreply.github.com>
2019-07-26 13:49:58 +0000

    Added inline documentation

--- tasks/deployment.py
+++ tasks/deployment.py
@@ -12,7 +12,7 @@ def iac_path(context):
   '''
   Returns the path of the deployment code inside this repository.
 
-  :param context: pyinvoke context
+  :param context: PyInvoke context
   :return: path of the deployment code
   '''
   path = 'terraform/modules/{}/deployments/{}'
@@ -21,9 +21,9 @@ def iac_path(context):
 
 def var_files(context):
   '''
-  Returns the list of the terraform variables files
+  Returns the list of the terraform variables files.
 
-  :param context: pyinvoke context
+  :param context: PyInvoke context
   :return: the list of the terraform variables files
   '''
   order = [
@@ -41,6 +41,14 @@ def var_files(context):
   return paths
 
 def tfplan_file(context, to):
+  '''
+  Returns the `tfplan` the direcotry path and the filename associated witht the
+  desired operation.
+
+  :param context: PyInvoke context
+  :param to: What the plan is about: either `create`, `destroy` or `update`
+  :return: tfplan directory path and the filename
+  '''
   return [
     os.path.join(
       'terraform', 'deployments',
@@ -53,6 +61,13 @@ def tfplan_file(context, to):
   ]
 
 def run_terraform(context, args):
+  '''
+  Prepares the environment variables and then runs the terraform command to
+  carry out the desired operation.
+
+  :param context: PyInvoke context
+  :param args: a string with all the terraform command arguments
+  '''
   env = {
     'TF_VAR_datacenter': context.config['meta']['datacenter'],
     'TF_VAR_programme': context.config['meta']['programme'],
@@ -68,6 +83,9 @@ def run_terraform(context, args):
 
 @invoke.task
 def clean(context):
+  '''
+  Removes all `tfstate` and `tfplan` files.
+  '''
   tfplans = os.path.join(*tfplan_file(context, '*'))
   tfstate = os.path.join('.terraform', 'terraform.tfstate')
   print('Removing {}'.format(tfplans))
@@ -79,6 +97,11 @@ def clean(context):
 
 @invoke.task(pre=[clean])
 def init(context):
+  '''
+  Runs `terraform init` which also configures the remote state backend.
+
+  :param context: PyInvoke context
+  '''
   tfstate = os.path.join('terraform', 'deployments',
                          context.config['meta']['datacenter'],
                          context.config['meta']['programme'],
@@ -102,11 +125,22 @@ def init(context):
 
 @invoke.task(init)
 def validate(context):
+  '''
+  Runs `terraform validate` on the specified provisioning module.
+
+  :param context: PyInvoke context
+  '''
   options = ' '.join(['-var-file={}'.format(f) for f in var_files(context)])
   run_terraform(context, 'validate -check-variables=false {} {}'.format(options, iac_path(context)))
 
 @invoke.task(validate)
 def plan(context, to='create'):
+  '''
+  Runs `terraform plan`. Different plans can be created..
+
+  :param context: PyInvoke context
+  :param to: What the plan is about: either `create`, `destroy` or `update`
+  '''
   _var_file = ['-var-file={}'.format(f) for f in var_files(context)]
 
   tfplan_dirname, tfplan_basename = tfplan_file(context, to)
@@ -120,6 +154,12 @@ def plan(context, to='create'):
   run_terraform(context, 'plan {} {}'.format(' '.join(options), iac_path(context)))
 
 def apply_plan(context, to):
+  '''
+  Applies a previously generated plan (with `terraform plan`).
+
+  :param context: PyInvoke context
+  :param to: What the plan is about: either `create`, `destroy` or `update`
+  '''
   tfplan = os.path.join(*tfplan_file(context, to))
   _parallelism = '-parallelism={}'.format(context.config['terraform']['parallelism'])
   if os.path.isfile(tfplan):
@@ -133,20 +173,46 @@ def apply_plan(context, to):
 
 @invoke.task(pre=[invoke.call(plan, to='create')])
 def deploy(context):
+  '''
+  Applies the plan to create a piece of infrastructure.
+
+  :param context: PyInvoke context
+  '''
   apply_plan(context, 'create')
 
 @invoke.task(pre=[invoke.call(plan, to='destroy')])
 def decommission(context):
+  '''
+  Applies the plan to destroy a piece of infrastructure.
+
+  :param context: PyInvoke context
+  '''
   apply_plan(context, 'destroy')
 
 @invoke.task(post=[deploy])
 def create(context, name, owner=None, parallelism=64):
+  '''
+  Front-end task that prepares the context for the `deploy` task.
+
+  :param context: PyInvoke context
+  :param name: name of the deployment to be created
+  :param owner: the name of the owner of the deployment. Defaults to current user
+  :param parallelism: the number of objects to be concurrently created
+  '''
   context.config['deployment']['name'] = name
   context.config['deployment']['owner'] = owner or os.environ['OS_USERNAME']
   context.config['terraform']['parallelism'] = parallelism
 
 @invoke.task(post=[decommission])
 def destroy(context, name, owner=None, parallelism=64):
+  '''
+  Front-end task that prepares the context for the `decommission` task.
+
+  :param context: PyInvoke context
+  :param name: name of the deployment to be destroyed
+  :param owner: the name of the owner of the deployment. Defaults to current user
+  :param parallelism: the number of objects to be concurrently destroyed
+  '''
   context.config['deployment']['name'] = name
   context.config['deployment']['owner'] = owner or os.environ['OS_USERNAME']
   context.config['terraform']['parallelism'] = parallelism
--- tasks/docker_image.py
+++ tasks/docker_image.py
@@ -1,3 +1,6 @@
+'''
+This group of tasks deals with the lifecycle of docker images.
+'''
 import datetime
 import glob
 import os
@@ -12,13 +15,25 @@ import botocore
 import invoke
 
 def get_bucket_name(context):
+  '''
+  Returns the bucket name for the current context.
+
+  :param context: PyInvoke context
+  :return: the name of the bucket
+  '''
   return '-'.join([
     context.config['meta']['datacenter'],
     context.config['meta']['programme'],
     'bucket',
     context.config['deployment']['owner']])
 
-def get_container_name(context):
+def get_image_name(context):
+  '''
+  Returns the container image name for the current context.
+
+  :param context: PyInvoke context
+  :return: the name of the container image
+  '''
   return '-'.join([context.config['meta']['datacenter'],
                    context.config['meta']['programme'],
                    'docker',
@@ -27,6 +42,12 @@ def get_container_name(context):
 
 @invoke.task
 def clean(context):
+  '''
+  Removes all the docker images that have been built and staged on the local
+  filesystem, as well as those loaded in the running docker service.
+
+  :param context: PyInvoke context
+  '''
   build_prefix = context.config['build_prefix']
   for path in glob.glob(os.path.join(build_prefix, 'containers', 'docker', '*')):
     if os.path.exists(path):
@@ -34,15 +55,29 @@ def clean(context):
       print('Removed {}'.format(path))
   if not os.path.isdir(build_prefix):
     os.makedirs(build_prefix)
-
   context.run('docker system prune --all --force')
   
 @invoke.task
 def validate(context):
+  '''
+  Validated the packer tempalte.
+
+  :param context: PyInvoke context
+  '''
   context.run('packer validate packer/docker.json')
 
 @invoke.task(pre=[validate])
 def save(context, role_name=None, role_version=None, on_error=None, force=None, debug=None):
+  '''
+  Builds a docker image and saves it on the local filesystem
+
+  :param context: PyInvoke context
+  :param role_name: the name of the role to be provisioned
+  :param role_version: the version of the role to be provisioned
+  :param on_error: see packer documentation
+  :param force: see packer documentation
+  :param debug: see packer documentation
+  '''
   build_prefix = context.config['build_prefix']
   if role_name is not None:
     context.config['role']['name'] = role_name
@@ -59,10 +94,10 @@ def save(context, role_name=None, role_version=None, on_error=None, force=None, 
   if not os.path.isdir(save_path):
     os.makedirs(save_path)
 
-  container_name = get_container_name(context)
-  saved_container = os.path.join(save_path, '{}.tar'.format(container_name))
+  image_name = get_image_name(context)
+  saved_image = os.path.join(save_path, '{}.tar'.format(image_name))
 
-  if not os.path.isfile(saved_container):
+  if not os.path.isfile(saved_image):
     var_file = 'packer/vars/{}-{}.json'.format(context.config['meta']['datacenter'],
                                         context.config['meta']['programme'])
     options = ' '.join([
@@ -78,25 +113,30 @@ def save(context, role_name=None, role_version=None, on_error=None, force=None, 
     context.run('docker save hgi/{}:{} --output {}'.format(
                 context.config['role']['name'],
                 context.config['role']['version'],
-                saved_container))
+                saved_image))
   else:
-    print("Skipping {}: container is already build".format(saved_container))
+    print("Skipping {}: container image is already built".format(saved_image))
 
 @invoke.task(pre=[save])
 def upload(context):
+  '''
+  Uploads the saved contaienr image to the bucket
+
+  :param context: PyInvoke context
+  '''
   upload_prefix = context.config['upload_prefix']
   build_prefix = context.config['build_prefix']
 
   bucket_name = get_bucket_name(context)
-  container_name = get_container_name(context)
+  image_name = get_image_name(context)
   # I could not make the checksum work
   for ext in ['tar']:
-    container_file = os.path.join('containers', 'docker', '{}.{}'.format(container_name, ext))
+    image_file = os.path.join('containers', 'docker', '{}.{}'.format(image_name, ext))
 
-    with open(os.path.join(build_prefix, container_file), 'br') as content:
+    with open(os.path.join(build_prefix, image_file), 'br') as content:
       body = content.read()
 
-    object_path = os.path.join(upload_prefix, container_file)
+    object_path = os.path.join(upload_prefix, image_file)
     object_name = 's3://{}/{}'.format(get_bucket_name(context), object_path)
     s3 = boto3.resource('s3', endpoint_url='https://{}'.format(os.environ['AWS_S3_ENDPOINT']))
     print('Uploading distribution: {}'.format(object_name))
@@ -104,6 +144,16 @@ def upload(context):
 
 @invoke.task(post=[upload])
 def create(context, role_name, role_version, user=None, on_error='cleanup', force=False, debug=False):
+  '''
+  Prepares the context for the `save` and `upload` tasks.
+
+  :param context: PyInvoke context
+  :param role_name: the name of the role to be provisioned
+  :param role_version: the version of the role to be provisioned
+  :param on_error: see packer documentation
+  :param force: see packer documentation
+  :param debug: see packer documentation
+  '''
   context.config['role']['name'] = role_name
   context.config['role']['version'] = role_version
   context.config['packer']['on_error'] = on_error
@@ -112,27 +162,27 @@ def create(context, role_name, role_version, user=None, on_error='cleanup', forc
   context.config['deployment']['owner'] = user or os.environ['OS_USERNAME']
 
   bucket_name = get_bucket_name(context)
-  container_name = get_container_name(context)
-  container_tar = os.path.join('containers', 'docker', '{}.tar'.format(container_name))
+  image_name = get_image_name(context)
+  image_tar = os.path.join('containers', 'docker', '{}.tar'.format(image_name))
 
   upload_prefix = context.config['upload_prefix']
   build_prefix = context.config['build_prefix']
 
-  object_path = os.path.join(upload_prefix, container_tar)
+  object_path = os.path.join(upload_prefix, image_tar)
   object_name = 's3://{}/{}'.format(bucket_name, object_path)
   s3 = boto3.resource('s3', endpoint_url='https://{}'.format(os.environ['AWS_S3_ENDPOINT']))
   try:
     s3.Object(bucket_name, object_path).load()
   except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
-      print('Docker container does not exist: {}'.format(object_name))
+      print('Docker container image does not exist: {}'.format(object_name))
     else:
       raise
   else:
     if force:
       print('Forcing the building process...')
     else:
-      print('Docker container already exists: {}'.format(object_name))
+      print('Docker container image already exists: {}'.format(object_name))
       sys.exit(0)
 
 ns = invoke.Collection()
