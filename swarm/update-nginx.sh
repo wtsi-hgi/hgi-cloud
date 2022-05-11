4121622bd6db5f16022dfaea61fec196379fa34d
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2022-05-11 15:26:25 +0100

    Update update-nginx.sh

--- swarm/update-nginx.sh
+++ swarm/update-nginx.sh
@@ -10,3 +10,6 @@ docker config rm ${stack}_nginx_config
 docker config create ${stack}_nginx_config /home/ubuntu/swarm/nginx.conf                                                                   
 docker service update --config-add source=${stack}_nginx_config,target=/etc/nginx/nginx.conf ${stack}_nginx                                
 docker service scale ${stack}_nginx=1     
+
+# refreshing all the services with info from docker-compose file: 
+docker stack deploy --with-registry-auth --compose-file swarm/docker-compose.yml dockerSwarm
