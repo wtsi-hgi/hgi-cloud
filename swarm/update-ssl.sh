73d44558a6657cbad0ed8c4046359dd10353e9af
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2022-05-11 15:24:51 +0100

    Create update-ssl.sh

---
+++ swarm/update-ssl.sh
@@ -0,0 +1,9 @@
+docker service scale ${stack}_nginx=0                                                                                                      
+docker service update --config-rm ${stack}_ssl_cert ${stack}_nginx                                                                         
+docker config rm ${stack}_ssl_cert                                                                                                         
+docker config create ${stack}_ssl_cert /home/ubuntu/swarm/apps_hgi_sanger_ac_uk-cert.pem                                                   
+docker service update --config-add source=${stack}_ssl_cert,target=/etc/nginx/etc/ssl/certs/apps_hgi_sanger_ac_uk-cert.pem ${stack}_nginx  
+docker service scale ${stack}_nginx=1 
+
+# refreshing all the services with info from docker-compose file: 
+docker stack deploy --with-registry-auth --compose-file swarm/docker-compose.yml dockerSwarm
